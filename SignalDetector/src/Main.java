9c809773141e6b4f2f2f9e270839bfde684b2fee
Marchinho93 <braminim@gmail.com>
2016-04-13 16:59:45 +0200

    SCR Project

---
+++ SignalDetector/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random random = new Random();
+		System.out.println(random.nextGaussian());
+	}
+
+}
